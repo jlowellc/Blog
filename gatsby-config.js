4c375f585a1a5d4e767cb69e880e29836e7c835b
James Carter <jlcarter.2342@outlook.com>
2020-02-24 21:15:20 -0600

    Added New Pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Code Blog`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `A blog to discuss projects as well as coding topics`,
     author: `James Carter`,
   },
   plugins: [
