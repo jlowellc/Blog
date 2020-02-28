3d9748026b3a509ed41ee211ffd6258c283b06db
James Carter <jlcarter.2342@outlook.com>
2020-02-28 09:30:33 -0600

    Added images to posts

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,10 +1,12 @@
 import React from "react"
 import { Link } from "gatsby"
 import { Card, CardTitle, CardText, CardSubtitle, CardBody } from "reactstrap"
+import Img from "gatsby-image"
 
-const Post = ({ title, author, date, path, body }) => {
+const Post = ({ title, author, date, path, body, fluid }) => {
   return (
     <Card>
+      <Img className="card-image-top" fluid={fluid} />
       <CardBody>
         <CardTitle>
           <Link to={path}>{title}</Link>
