ef3fd784e3e37bb1e53623945c397fa2f0b58e4b
James Carter <jlcarter.2342@outlook.com>
2020-02-28 09:58:24 -0600

    Created tags

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,9 +1,17 @@
 import React from "react"
 import { Link } from "gatsby"
-import { Card, CardTitle, CardText, CardSubtitle, CardBody } from "reactstrap"
+import {
+  Badge,
+  Card,
+  CardTitle,
+  CardText,
+  CardSubtitle,
+  CardBody,
+} from "reactstrap"
 import Img from "gatsby-image"
+import { slugify } from "../util/utility"
 
-const Post = ({ title, author, date, path, body, fluid }) => {
+const Post = ({ title, author, date, path, body, fluid, tags }) => {
   return (
     <Card>
       <Img className="card-image-top" fluid={fluid} />
@@ -16,6 +24,17 @@ const Post = ({ title, author, date, path, body, fluid }) => {
           <span className="text-info">{author}</span>
         </CardSubtitle>
         <CardText>{body}</CardText>
+        <ul className="post-tags">
+          {tags.map(tags => (
+            <li>
+              <Link to={`/tag/${slugify(tags)}`}>
+                <Badge color="primary" className="text-uppercase">
+                  {tags}
+                </Badge>
+              </Link>
+            </li>
+          ))}
+        </ul>
         <Link to={path} className="btn btn-outline-primary float-right">
           Read more
         </Link>
