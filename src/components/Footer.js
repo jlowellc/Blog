3a38bca1e51dc3df23b4accbdb74b3a702323452
James Carter <jlcarter.2342@outlook.com>
2020-02-28 15:12:05 -0600

    Created a footer and sidebar posts

---
+++ src/components/Footer.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+const Footer = () => (
+  <div className="site-footer">
+    <h4 className="text-center">Code Blog</h4>
+    <p className="text-center">Follow me on social media</p>
+    <div className="footer-social-links">
+      <ul className="social-links-list">
+        <li>
+          <a
+            href="http://www.facebook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="facebook"
+          >
+            <i class="fab fa-facebook-f"></i>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+)
+
+export default Footer
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,30 +1,33 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
+import Footer from "./Footer"
 import "../styles/index.scss"
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
+const Layout = ({ children }) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
         }
       }
-    }
-  `)
-
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div className="container" id="content">
-        <main>{children}</main>
-      </div>
-    </>
-  )
-}
+    `}
+    render={data => (
+      <>
+        <Header siteTitle={data.site.siteMetadata.title} />
+        <div className="container" id="content">
+          <main>{children}</main>
+        </div>
+        <Footer />
+      </>
+    )}
+  />
+)
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
